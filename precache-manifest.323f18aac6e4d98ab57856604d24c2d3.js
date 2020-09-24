74bd30724bbe8caae6e08e5272beebaf3c19ff65
Rachel Ouday <rachel@enroute.tech>
2020-09-24 15:53:44 +0300

    Updates

---
+++ precache-manifest.323f18aac6e4d98ab57856604d24c2d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d00e0e6daab75faee895a1a1c32b2db",
+    "url": "/Rachel-Ouday-20-09-2020/index.html"
+  },
+  {
+    "revision": "19436ae9d01df60363c7",
+    "url": "/Rachel-Ouday-20-09-2020/static/css/2.41fc952c.chunk.css"
+  },
+  {
+    "revision": "7f7829b61dea9b576397",
+    "url": "/Rachel-Ouday-20-09-2020/static/css/main.e0bbd5ac.chunk.css"
+  },
+  {
+    "revision": "19436ae9d01df60363c7",
+    "url": "/Rachel-Ouday-20-09-2020/static/js/2.f38c17c1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Rachel-Ouday-20-09-2020/static/js/2.f38c17c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f7829b61dea9b576397",
+    "url": "/Rachel-Ouday-20-09-2020/static/js/main.cfb8da7c.chunk.js"
+  },
+  {
+    "revision": "4359e6e705e0df582dd8",
+    "url": "/Rachel-Ouday-20-09-2020/static/js/runtime-main.962aba27.js"
+  }
+]);
